3ef56b9dbf017beb4b08ba5e17b4933499bb8af1
Nahin <nahinkhanofficial@gmail.com>
2025-11-13 15:26:08 -0500

    3.1-3.3

---
+++ ListPrac1.py
@@ -0,0 +1,11 @@
+names = ['Aabid', 'Tasin', 'Sameen', 'LeBron', 'Kobe']
+
+for name in names:
+    print(f"Greetings, {name}!")
+
+transportation_modes = ['foot', 'bike', 'truck', 'bus']
+
+print(f"I can't walk on my {transportation_modes[0]}.")
+print(f'I like to ride my {transportation_modes[1]}.')
+print(f'My {transportation_modes[2]} is very big.')
+print(f'The {transportation_modes[3]} is arriving soon.')
