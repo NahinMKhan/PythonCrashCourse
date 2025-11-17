beeff4443792b53528018a208b64c0331da409a0
Nahin <nahinkhanofficial@gmail.com>
2025-11-16 21:38:55 -0500

    3.8

---
+++ chap3/3-8SeeingWorld.py
@@ -0,0 +1,16 @@
+places = ['Dubai', 'Barcelona', 'Brazil', 'Tokyo', 'Morocco']
+print(places)
+print(f'\n{sorted(places)} \n')
+print(places)
+
+places.reverse()
+print(f'\n{places}')
+
+places.reverse()
+print(f'\n{places}')
+
+places.sort()
+print(f'\n{places}')
+
+places.sort(reverse=True)
+print(f'\n{places}')
