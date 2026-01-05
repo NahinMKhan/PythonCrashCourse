0c58ebbc5ae2f000e8bd68615e5b6f6dd14d8c11
Nahin Khan <nahinkhanofficial@gmail.com>
2026-01-05 17:17:36 -0500

    4-13

---
+++ chap4/4-13Buffet.py
@@ -0,0 +1,10 @@
+foods = ('burger', 'fries', 'coke', 'salad', 'porkchops')
+for food in foods:
+    print(food)
+
+food[0] = 'coleslaw'
+
+foods = ('grilled chicken', 'loaded fries', 'dino nuggies', 'noodles', 'alcohol')
+for food in foods:
+    print(food)
+
